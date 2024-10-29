bf53e9b87d8195c66cc9cf03c5055b2a256709e6
bidrihi <bidrihi@gmail.com>
2024-10-29 15:15:16 +0900

    메뉴바 작성

--- src/rhythm/DynamicBeat.java
+++ src/rhythm/DynamicBeat.java
@@ -2,6 +2,9 @@ package rhythm;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class DynamicBeat extends JFrame {
     public static final int SCREEN_WIDTH = 1280;
@@ -10,17 +13,66 @@ public class DynamicBeat extends JFrame {
     private Image screenImage;
     private Graphics screenGraphic;
 
-    private Image introBackground;
+    private final Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(Title).jpg")).getImage();
+    private final JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+    private final ImageIcon exitButtonEnterdImage = new ImageIcon(Main.class.getResource("../images/exit_entered.png"));
+    private final ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exit.png"));
+    private JButton exitButton = new JButton(exitButtonBasicImage);
+
+    private int mouseX, mouseY;
 
     public DynamicBeat() {
+        setUndecorated(true); // 기본 메뉴바 숨기고 바뀐 메뉴바로 변경
         setTitle("Dynamic Beat");
         setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         setResizable(false); // 창 크기 변경 불가
         setLocationRelativeTo(null); // 실행시 화면 정중앙에 뜨도록 설정
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        setBackground(new Color(0, 0, 0, 0));
+        setLayout(null);
 
-        introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(Title).jpg")).getImage();
+        exitButton.setBounds(1245, 0, 30, 30);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
+        exitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                exitButton.setIcon(exitButtonEnterdImage);
+                exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                exitButton.setIcon(exitButtonBasicImage);
+                exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        add(exitButton);
+
+        menuBar.setBounds(0, 0, 1280, 30);
+        menuBar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+        menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = e.getXOnScreen();
+                int y = e.getYOnScreen();
+                setLocation(x - mouseX, y - mouseY);
+            }
+        });
+        add(menuBar);
 
         Music introMusic = new Music("upbeat-reel.mp3", true);
         introMusic.start();
@@ -44,6 +96,7 @@ public class DynamicBeat extends JFrame {
 
     public void screenDraw(Graphics g) {
         g.drawImage(introBackground, 0, 0, null);
+        paintComponents(g); // 이미지를 단순히 그려주는 것 이외에 JLabel 처럼 추가된 요소를 그리는 것
         this.repaint();
     }
 }
