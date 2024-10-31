c2d725316ec1663a61d835636ee8f3341ff54d66
bidrihi <bidrihi@gmail.com>
2024-10-31 10:05:10 +0900

    키보드 이벤트

--- src/rhythm/DynamicBeat.java
+++ src/rhythm/DynamicBeat.java
@@ -42,11 +42,6 @@ public class DynamicBeat extends JFrame {
     private final ImageIcon backBasicImage = new ImageIcon(Main.class.getResource("../images/backButtonBasic.png"));
     private final JButton backButton = new JButton(backBasicImage);
 
-    private final Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage();
-    private final Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
-    private final Image noteRouteImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
-    private final Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage();
-    private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.jpg")).getImage();
     private boolean isGameScreen = false;
 
     private int mouseX, mouseY;
@@ -60,6 +55,8 @@ public class DynamicBeat extends JFrame {
     private Music selectedMusic;
     private int nowSelected = 0;
 
+    public static Game game = new Game();
+
     public DynamicBeat() {
         setUndecorated(true); // 기본 메뉴바 숨기고 바뀐 메뉴바로 변경
         setTitle("Dynamic Beat");
@@ -70,6 +67,8 @@ public class DynamicBeat extends JFrame {
         setVisible(true);
         setBackground(new Color(0, 0, 0, 0));
         setLayout(null);
+
+        addKeyListener(new KeyListener());
 
         introMusic.start();
 
@@ -324,49 +323,7 @@ public class DynamicBeat extends JFrame {
             g.drawImage(titleImage, 340, 550, null);
         }
         if (isGameScreen) {
-            g.drawImage(noteRouteImage, 228, 30, null);
-            g.drawImage(noteRouteImage, 332, 30, null);
-            g.drawImage(noteRouteImage, 436, 30, null);
-            g.drawImage(noteRouteImage, 540, 30, null);
-            g.drawImage(noteRouteImage, 640, 30, null);
-            g.drawImage(noteRouteImage, 744, 30, null);
-            g.drawImage(noteRouteImage, 848, 30, null);
-            g.drawImage(noteRouteImage, 952, 30, null);
-            g.drawImage(noteRouteLineImage, 224, 30, null);
-            g.drawImage(noteRouteLineImage, 328, 30, null);
-            g.drawImage(noteRouteLineImage, 432, 30, null);
-            g.drawImage(noteRouteLineImage, 536, 30, null);
-            g.drawImage(noteRouteLineImage, 740, 30, null);
-            g.drawImage(noteRouteLineImage, 844, 30, null);
-            g.drawImage(noteRouteLineImage, 948, 30, null);
-            g.drawImage(noteRouteLineImage, 1052, 30, null);
-            g.drawImage(gameInfoImage, 0, 660, null);
-            g.drawImage(judgementLineImage, 0, 580, null);
-            g.drawImage(noteBasicImage, 228, 120, null);
-            g.drawImage(noteBasicImage, 332, 580, null);
-            g.drawImage(noteBasicImage, 436, 500, null);
-            g.drawImage(noteBasicImage, 540, 340, null);
-            g.drawImage(noteBasicImage, 640, 340, null);
-            g.drawImage(noteBasicImage, 744, 325, null);
-            g.drawImage(noteBasicImage, 848, 305, null);
-            g.drawImage(noteBasicImage, 952, 305, null);
-            g.setColor(Color.white);
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g.setFont(new Font("Arial", Font.BOLD, 30));
-            g.drawString("INFORMATION", 20, 702);
-            g.drawString("Easy", 1190, 702);
-            g.setFont(new Font("Arial", Font.PLAIN, 26));
-            g.setColor(Color.DARK_GRAY);
-            g.drawString("S", 270, 609);
-            g.drawString("D", 374, 609);
-            g.drawString("F", 478, 609);
-            g.drawString("Space Bar", 580, 609);
-            g.drawString("J", 784, 609);
-            g.drawString("K", 889, 609);
-            g.drawString("L", 993, 609);
-            g.setFont(new Font("Elephant", Font.BOLD, 30));
-            g.drawString("000000", 565, 702);
-
+            game.screenDraw(g);
         }
         paintComponents(g); // 이미지를 단순히 그려주는 것 이외에 JLabel 처럼 추가된 요소를 그리는 것
         this.repaint();
@@ -412,6 +369,7 @@ public class DynamicBeat extends JFrame {
         background = new ImageIcon(Main.class.getResource("../images/" + tracklist.get(nowSelected).getGameImage())).getImage();
         backButton.setVisible(true);
         isGameScreen = true;
+        setFocusable(true);
     }
 
     public void backMain() {
